3328a257fa06c57e6146ec42645508bfcb734352
Sami Mäkelä <sami@httpster.io>
2025-11-01 13:27:30 +0200

    Auto backup: 2025-11-01

--- autopush.sh
+++ autopush.sh
@@ -7,14 +7,20 @@ REPO="$HOME/.itgmania/Save"
 
 cd "$REPO" || exit
 
-#Check for changes
-if git status --porcelain | grep -q .; then
-	git add .
-	git commit -m "Auto backup: $(date '+%Y-%m-%d')"
-	git push --force
-	echo "finished backing up itgmania"
+rm -f .git/index.lock
+git merge --abort 2>/dev/null || true
+git rebase --abort 2>/dev/null || true
 
-else 
-	echo "no changes"
-	exit 0
-fi
+timeout 120 bash -c '
+#Check for changes
+	if git status --porcelain | grep -q .; then
+		git add .
+		git commit -m "Auto backup: $(date '+%Y-%m-%d')"
+		git push --force
+		echo "finished backing up itgmania"
+
+	else 
+		echo "no changes"
+		exit 0
+	fi
+'
